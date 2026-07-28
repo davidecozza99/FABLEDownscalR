3ae8433b2fde85e871be96dc2c43dae0d4d0d216
davidecozza99 <davide.cozza@unsdsn.org>
2026-07-28 17:13:51 +0200

    Update plt_maps.R

--- R/plt_maps.R
+++ R/plt_maps.R
@@ -254,131 +254,14 @@ fdr_plot_downscaled_LU_one <- function(
   return(p)
 }
 
-
-# LAND USE (multiple maps)
-fdr_plot_downscaled_LU <- function(
-    out_res,
-    rasterized_layer,
-    ns_map,
-    border_sf  = NULL,
-    year       = c(2020, 2050),
-    LU         = NULL,
-    limits     = NULL,
-    na_color   = "grey90",
-    add_border = TRUE
-) {
-  chk_required_cols(out_res, c("ns", "lu.to", "times", "value"))
-  out_int <- fdr_to_ns_int(out_res, ns_map)
-
-  df_pix <- terra::as.data.frame(rasterized_layer, xy = TRUE, na.rm = FALSE)
-  names(df_pix)[3] <- "ns"
-  df_pix <- dplyr::filter(df_pix, !is.na(ns))
-
-  inputs <- out_int %>%
-    dplyr::group_by(ns, lu.to, times) %>%
-    dplyr::summarise(value = sum(value), .groups = "drop")
-
-  if (!is.null(LU))   inputs <- inputs %>% dplyr::filter(lu.to %in% LU)
-  if (!is.null(year)) inputs <- inputs %>% dplyr::filter(times %in% year)
-
-  plot_df <- df_pix %>%
-    dplyr::left_join(inputs, by = "ns") %>%
-    dplyr::filter(!is.na(lu.to), !is.na(times))
-
-  lu_order <- c("newforest", "cropland", "otherland", "forest", "pasture")
-  plot_df$lu.to <- factor(plot_df$lu.to, levels = lu_order)
-  lu_present <- na.omit(unique(as.character(plot_df$lu.to)))
-
-  if (is.null(limits)) limits <- range(plot_df$value, na.rm = TRUE)
-
-  plot_df <- plot_df %>%
-    dplyr::mutate(times = factor(times, levels = sort(unique(as.numeric(as.character(times))))))
-
-  lu_colors <- list(
-    cropland  = "#FFA500",
-    forest    = "#00B300",
-    newforest = "#7CFC00",
-    otherland = "#A020F0",
-    pasture   = "#FF0000",
-    urban     = "#A9A9A9"
-  )
-  lu_labels <- c(
-    cropland  = "Cropland",
-    forest    = "Forest",
-    newforest = "New forest",
-    otherland = "Other land",
-    pasture   = "Pasture",
-    urban     = "Urban"
-  )
-
-  library(ggnewscale)
-  p <- ggplot2::ggplot()
-  for (i in seq_along(lu_present)) {
-    lu <- lu_present[i]
-    p <- p +
-      ggplot2::geom_raster(
-        data = dplyr::filter(plot_df, lu.to == lu),
-        ggplot2::aes(x = x, y = y, fill = value)
-      ) +
-      ggplot2::scale_fill_gradient(
-        low      = "white",
-        high     = lu_colors[[lu]],
-        limits   = limits,
-        na.value = na_color,
-        name     = paste0(lu_labels[[lu]], " (1000 ha)")
-      )
-    if (i < length(lu_present)) p <- p + ggnewscale::new_scale_fill()
-  }
-
-  p <- p +
-    ggplot2::coord_equal(expand = FALSE) +
-    theme_fdr_map() +
-    ggplot2::facet_grid(
-      . ~ times + lu.to,
-      labeller = ggplot2::labeller(lu.to = lu_labels)
-    )
-
-  # ----------------------------
-  # Border + white mask outside
-  # ----------------------------
-  if (add_border) {
-    if (!is.null(border_sf)) {
-      raster_crs <- terra::crs(rasterized_layer)
-      border_use <- sf::st_transform(border_sf, crs = raster_crs)
-    } else {
-      r          <- terra::app(rasterized_layer, function(x) ifelse(is.na(x), NA, 1))
-      border_use <- sf::st_as_sf(terra::as.polygons(r, dissolve = TRUE))
-    }
-
-    bbox_poly    <- sf::st_as_sfc(sf::st_bbox(border_use))
-    outside_poly <- sf::st_difference(bbox_poly, sf::st_union(border_use))
-
-    p <- p +
-      ggplot2::geom_sf(
-        data      = outside_poly,
-        fill      = "white",
-        color     = NA,
-        linewidth = 0
-      ) +
-      ggplot2::geom_sf(
-        data      = border_use,
-        fill      = NA,
-        color     = "grey60",
-        linewidth = 0.3
-      )
-  }
-
-  return(p)
-}
-
-
-# # LAND USE CHANGE (multiple maps)
-# fdr_plot_downscaled_LUC <- function(
+#  FOR NEW FOREST
+# # LAND USE (multiple maps)
+# fdr_plot_downscaled_LU <- function(
 #     out_res,
 #     rasterized_layer,
 #     ns_map,
 #     border_sf  = NULL,
-#     year       = NULL,
+#     year       = c(2020, 2050),
 #     LU         = NULL,
 #     limits     = NULL,
 #     na_color   = "grey90",
@@ -386,31 +269,39 @@ fdr_plot_downscaled_LU <- function(
 # ) {
 #   chk_required_cols(out_res, c("ns", "lu.to", "times", "value"))
 #   out_int <- fdr_to_ns_int(out_res, ns_map)
+#
 #   df_pix <- terra::as.data.frame(rasterized_layer, xy = TRUE, na.rm = FALSE)
 #   names(df_pix)[3] <- "ns"
 #   df_pix <- dplyr::filter(df_pix, !is.na(ns))
+#
 #   inputs <- out_int %>%
-#     dplyr::filter(lu.from != lu.to) %>%
-#     dplyr::group_by(lu.to, ns, times) %>%
-#     dplyr::summarise(gain = sum(value, na.rm = TRUE), .groups = "drop") %>%
-#     dplyr::mutate(Type = "gain", lu = lu.to) %>%
-#     dplyr::bind_rows(
-#       out_int %>%
-#         dplyr::filter(lu.from != lu.to) %>%
-#         dplyr::group_by(lu.from, ns, times) %>%
-#         dplyr::summarise(loss = -sum(value, na.rm = TRUE), .groups = "drop") %>%
-#         dplyr::mutate(Type = "loss", lu = lu.from)
-#     ) %>%
-#     dplyr::group_by(lu, times, ns) %>%
-#     dplyr::summarise(value = sum(gain, loss, na.rm = TRUE), .groups = "drop") %>%
-#     dplyr::rename(lu.to = lu)
+#     dplyr::group_by(ns, lu.to, times) %>%
+#     dplyr::summarise(value = sum(value), .groups = "drop")
+#
 #   if (!is.null(LU))   inputs <- inputs %>% dplyr::filter(lu.to %in% LU)
 #   if (!is.null(year)) inputs <- inputs %>% dplyr::filter(times %in% year)
+#
 #   plot_df <- df_pix %>%
 #     dplyr::left_join(inputs, by = "ns") %>%
 #     dplyr::filter(!is.na(lu.to), !is.na(times))
-#   lu_order <- c("cropland", "newforest", "otherland", "pasture", "forest", "urban")
+#
+#   lu_order <- c("newforest", "cropland", "otherland", "forest", "pasture")
 #   plot_df$lu.to <- factor(plot_df$lu.to, levels = lu_order)
+#   lu_present <- na.omit(unique(as.character(plot_df$lu.to)))
+#
+#   if (is.null(limits)) limits <- range(plot_df$value, na.rm = TRUE)
+#
+#   plot_df <- plot_df %>%
+#     dplyr::mutate(times = factor(times, levels = sort(unique(as.numeric(as.character(times))))))
+#
+#   lu_colors <- list(
+#     cropland  = "#FFA500",
+#     forest    = "#00B300",
+#     newforest = "#7CFC00",
+#     otherland = "#A020F0",
+#     pasture   = "#FF0000",
+#     urban     = "#A9A9A9"
+#   )
 #   lu_labels <- c(
 #     cropland  = "Cropland",
 #     forest    = "Forest",
@@ -419,27 +310,34 @@ fdr_plot_downscaled_LU <- function(
 #     pasture   = "Pasture",
 #     urban     = "Urban"
 #   )
-#   if (is.null(limits)) {
-#     max_abs <- max(abs(plot_df$value), na.rm = TRUE)
-#     limits  <- c(-max_abs, max_abs)
+#
+#   library(ggnewscale)
+#   p <- ggplot2::ggplot()
+#   for (i in seq_along(lu_present)) {
+#     lu <- lu_present[i]
+#     p <- p +
+#       ggplot2::geom_raster(
+#         data = dplyr::filter(plot_df, lu.to == lu),
+#         ggplot2::aes(x = x, y = y, fill = value)
+#       ) +
+#       ggplot2::scale_fill_gradient(
+#         low      = "white",
+#         high     = lu_colors[[lu]],
+#         limits   = limits,
+#         na.value = na_color,
+#         name     = paste0(lu_labels[[lu]], " (1000 ha)")
+#       )
+#     if (i < length(lu_present)) p <- p + ggnewscale::new_scale_fill()
 #   }
-#   p <- ggplot2::ggplot(plot_df) +
-#     ggplot2::geom_raster(ggplot2::aes(x = x, y = y, fill = value)) +
-#     ggplot2::scale_fill_gradient2(
-#       low      = "#FF0000",
-#       mid      = "white",
-#       high     = "#00B300",
-#       midpoint = 0,
-#       limits   = limits,
-#       na.value = na_color,
-#       name     = "1000 ha"
-#     ) +
+#
+#   p <- p +
 #     ggplot2::coord_equal(expand = FALSE) +
 #     theme_fdr_map() +
 #     ggplot2::facet_grid(
-#       lu.to ~ times,
+#       . ~ times + lu.to,
 #       labeller = ggplot2::labeller(lu.to = lu_labels)
 #     )
+#
 #   # ----------------------------
 #   # Border + white mask outside
 #   # ----------------------------
@@ -451,8 +349,10 @@ fdr_plot_downscaled_LU <- function(
 #       r          <- terra::app(rasterized_layer, function(x) ifelse(is.na(x), NA, 1))
 #       border_use <- sf::st_as_sf(terra::as.polygons(r, dissolve = TRUE))
 #     }
+#
 #     bbox_poly    <- sf::st_as_sfc(sf::st_bbox(border_use))
 #     outside_poly <- sf::st_difference(bbox_poly, sf::st_union(border_use))
+#
 #     p <- p +
 #       ggplot2::geom_sf(
 #         data      = outside_poly,
@@ -467,8 +367,108 @@ fdr_plot_downscaled_LU <- function(
 #         linewidth = 0.3
 #       )
 #   }
+#
 #   return(p)
 # }
+
+
+# LAND USE CHANGE (multiple maps)
+fdr_plot_downscaled_LUC <- function(
+    out_res,
+    rasterized_layer,
+    ns_map,
+    border_sf  = NULL,
+    year       = NULL,
+    LU         = NULL,
+    limits     = NULL,
+    na_color   = "grey90",
+    add_border = TRUE
+) {
+  chk_required_cols(out_res, c("ns", "lu.to", "times", "value"))
+  out_int <- fdr_to_ns_int(out_res, ns_map)
+  df_pix <- terra::as.data.frame(rasterized_layer, xy = TRUE, na.rm = FALSE)
+  names(df_pix)[3] <- "ns"
+  df_pix <- dplyr::filter(df_pix, !is.na(ns))
+  inputs <- out_int %>%
+    dplyr::filter(lu.from != lu.to) %>%
+    dplyr::group_by(lu.to, ns, times) %>%
+    dplyr::summarise(gain = sum(value, na.rm = TRUE), .groups = "drop") %>%
+    dplyr::mutate(Type = "gain", lu = lu.to) %>%
+    dplyr::bind_rows(
+      out_int %>%
+        dplyr::filter(lu.from != lu.to) %>%
+        dplyr::group_by(lu.from, ns, times) %>%
+        dplyr::summarise(loss = -sum(value, na.rm = TRUE), .groups = "drop") %>%
+        dplyr::mutate(Type = "loss", lu = lu.from)
+    ) %>%
+    dplyr::group_by(lu, times, ns) %>%
+    dplyr::summarise(value = sum(gain, loss, na.rm = TRUE), .groups = "drop") %>%
+    dplyr::rename(lu.to = lu)
+  if (!is.null(LU))   inputs <- inputs %>% dplyr::filter(lu.to %in% LU)
+  if (!is.null(year)) inputs <- inputs %>% dplyr::filter(times %in% year)
+  plot_df <- df_pix %>%
+    dplyr::left_join(inputs, by = "ns") %>%
+    dplyr::filter(!is.na(lu.to), !is.na(times))
+  lu_order <- c("cropland", "newforest", "otherland", "pasture", "forest", "urban")
+  plot_df$lu.to <- factor(plot_df$lu.to, levels = lu_order)
+  lu_labels <- c(
+    cropland  = "Cropland",
+    forest    = "Forest",
+    newforest = "New forest",
+    otherland = "Other land",
+    pasture   = "Pasture",
+    urban     = "Urban"
+  )
+  if (is.null(limits)) {
+    max_abs <- max(abs(plot_df$value), na.rm = TRUE)
+    limits  <- c(-max_abs, max_abs)
+  }
+  p <- ggplot2::ggplot(plot_df) +
+    ggplot2::geom_raster(ggplot2::aes(x = x, y = y, fill = value)) +
+    ggplot2::scale_fill_gradient2(
+      low      = "#FF0000",
+      mid      = "white",
+      high     = "#00B300",
+      midpoint = 0,
+      limits   = limits,
+      na.value = na_color,
+      name     = "1000 ha"
+    ) +
+    ggplot2::coord_equal(expand = FALSE) +
+    theme_fdr_map() +
+    ggplot2::facet_grid(
+      lu.to ~ times,
+      labeller = ggplot2::labeller(lu.to = lu_labels)
+    )
+  # ----------------------------
+  # Border + white mask outside
+  # ----------------------------
+  if (add_border) {
+    if (!is.null(border_sf)) {
+      raster_crs <- terra::crs(rasterized_layer)
+      border_use <- sf::st_transform(border_sf, crs = raster_crs)
+    } else {
+      r          <- terra::app(rasterized_layer, function(x) ifelse(is.na(x), NA, 1))
+      border_use <- sf::st_as_sf(terra::as.polygons(r, dissolve = TRUE))
+    }
+    bbox_poly    <- sf::st_as_sfc(sf::st_bbox(border_use))
+    outside_poly <- sf::st_difference(bbox_poly, sf::st_union(border_use))
+    p <- p +
+      ggplot2::geom_sf(
+        data      = outside_poly,
+        fill      = "white",
+        color     = NA,
+        linewidth = 0
+      ) +
+      ggplot2::geom_sf(
+        data      = border_use,
+        fill      = NA,
+        color     = "grey60",
+        linewidth = 0.3
+      )
+  }
+  return(p)
+}
 
 
 # LAND USE CHANGE (multiple maps)
